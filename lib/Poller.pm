f1eb4e4f50fc190469603b4a283760cd57cd4b94
Александр Поволоцкий <tarkhil@monitor.infotel.ru>
2014-07-23 23:39:10 +0400

    Don't just fail on timeout

--- lib/Poller.pm
+++ lib/Poller.pm
@@ -90,7 +90,7 @@ sub requests {
   my $polls = $device->get_pollers();
   if ( exists $device->{snmp_error} ) {
     print "Device ", $device->device_id, " (", $device->hostname, ") got error: ", $device->{snmp_error}, "\n";
-    return 0;
+#    return 0;
   }
   map { $self->plugin_hash->{$_}->request( $device ); } @$polls;
 }
